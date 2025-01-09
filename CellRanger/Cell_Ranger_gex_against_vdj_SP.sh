2201db3f68107bc8e3b29cf437e4de299ab3a075
JavierMolinaV <150247451+JavierMolinaV@users.noreply.github.com>
2025-01-09 14:22:37 +0100

    Add files via upload

---
+++ CellRanger/Cell_Ranger_gex_against_vdj_SP.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+#$ -P ARR
+#$ -A jmolinav
+#$ -N A12_SP_VDJ_new
+#$ -V
+#$ -pe pthreads 20
+#$ -cwd
+#$ -l h_vmem=5G
+# #$ -o /data_lab_ARR/arodriguezr/A12_10x/out/
+# #$ -e /data_lab_ARR/arodriguezr/A12_10x/error/
+# #$ -S "/usr/bin/env bash"
+
+
+/data_lab_ARR/arodriguezr/2.Cell_ranger/Data/cellranger-7.1.0/bin/cellranger count --id=A12_SP --transcriptome /data_lab_ARR/arodriguezr/2.Cell_ranger/Data/New_ref_A12_16_09_24 --fastqs /data_lab_ARR/arodriguezr/2.Cell_ranger/Data_A12/SP_VDJ --sample SP_VDJ 
