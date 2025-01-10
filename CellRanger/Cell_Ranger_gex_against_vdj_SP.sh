48a0cddf993acd439676cf0bb6e88c0a401aeb5e
JavierMolinaV <150247451+JavierMolinaV@users.noreply.github.com>
2025-01-10 10:49:22 +0100

    Update Cell_Ranger_gex_against_vdj_SP.sh

--- CellRanger/Cell_Ranger_gex_against_vdj_SP.sh
+++ CellRanger/Cell_Ranger_gex_against_vdj_SP.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 #$ -P ARR
-#$ -A jmolinav
-#$ -N A12_SP_VDJ_new
+#$ -A arodriguezr
+#$ -N A12_exp_10x_SP_ClusterJob
 #$ -V
 #$ -pe pthreads 20
 #$ -cwd
@@ -12,4 +12,4 @@
 # #$ -S "/usr/bin/env bash"
 
 
-/data_lab_ARR/arodriguezr/2.Cell_ranger/Data/cellranger-7.1.0/bin/cellranger count --id=A12_SP --transcriptome /data_lab_ARR/arodriguezr/2.Cell_ranger/Data/New_ref_A12_16_09_24 --fastqs /data_lab_ARR/arodriguezr/2.Cell_ranger/Data_A12/SP_VDJ --sample SP_VDJ 
+/data_lab_ARR/arodriguezr/2.Cell_ranger/Data/cellranger-7.1.0/bin/cellranger multi --id=A12_SP --csv=/data_lab_ARR/arodriguezr/2.Cell_ranger/Data_A12/multi_config_A12_exp_10x_SP_mod_ref.csv --jobmode=local --localcores=4
